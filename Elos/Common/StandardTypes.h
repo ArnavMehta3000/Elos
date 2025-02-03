7c0af977242fc50023156a639fb65f9c213e5878
Arnav Mehta <arnavdatt@gmail.com>
2025-02-03 20:37:06 +0000

    Add char types

--- Elos/Common/StandardTypes.h
+++ Elos/Common/StandardTypes.h
@@ -4,15 +4,17 @@
 
 namespace Elos
 {
-	using i8   = std::int8_t;
-	using i16  = std::int16_t;
-	using i32  = std::int32_t;
-	using i64  = std::int64_t;
-	using u8   = std::uint8_t;
-	using u16  = std::uint16_t;
-	using u32  = std::uint32_t;
-	using u64  = std::uint64_t;
-	using byte = std::byte;
-	using f32  = float;
-	using f64  = double;
+	using i8     = std::int8_t;
+	using i16    = std::int16_t;
+	using i32    = std::int32_t;
+	using i64    = std::int64_t;
+	using u8     = std::uint8_t;
+	using u16    = std::uint16_t;
+	using u32    = std::uint32_t;
+	using u64    = std::uint64_t;
+	using byte   = std::byte;
+	using char16 = char16_t;
+	using char32 = char32_t;
+	using f32    = float;
+	using f64    = double;
 }
