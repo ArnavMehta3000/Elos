926d77afd119d02b1b2876a2c76e9384575f5e0e
Arnav Mehta <arnavdatt@gmail.com>
2025-02-03 20:37:29 +0000

    Add function attribute macros

---
+++ Elos/Common/FunctionMacros.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define NORETURN [[noreturn]]
+#define MAYBE_UNUSED [[maybe_unused]]
+#define DEPRECATED [[deprecated]]
+#define NODISCARD [[nodiscard]]
+#define LIKELY [[likely]]
+#define UNLIKELY [[unlikely]]
